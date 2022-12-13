583ef84c27b01bd4299c3a5ecfadd6b300b04e7e
ahmedlhassan <a.ha99an@gmail.com>
2022-12-13 18:38:32 +0200

    edit models files

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
-
+from home.models import Tags
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(null=True, blank=True, upload_to="images/articles")
     snippet = models.TextField(null=True, blank=True)
     content = RichTextUploadingField()
+    tag = models.ManyToManyField(Tags, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
