ba3fdba78311b1fb276c409bee9df4fe8d24c0f8
yasserAboelgheit <yasser@huspy.io>
2022-12-13 14:01:56 +0000

    add docker

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
+from home.models import Tags
 
 
 class Article(models.Model):
@@ -7,8 +8,10 @@ class Article(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to="images/articles")
     snippet = models.TextField(null=True, blank=True)
     content = RichTextUploadingField()
+    tag = models.ManyToManyField(Tags, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    # article_type = models.char
 
     @property
     def get_snippet(self):
