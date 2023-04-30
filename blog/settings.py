f1bc60e7c5e954439b0d72ad44f3513844f5b567
yasserAboelgheit <yasser@huspy.io>
2023-04-30 17:26:00 +0400

    add whitenoise

--- blog/settings.py
+++ blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'solo.apps.SoloAppConfig',
     'ckeditor',
     'ckeditor_uploader',
+    'whitenoise.runserver_nostatic',
     
     'home',
     'articles',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
