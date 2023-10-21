c452284e2bb4fcbc19f6192fd5eecf27471a4622
yasserAboelgheit <yasser@huspy.io>
2023-10-21 22:06:23 +0400

    only 6 articles in home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView, CreateView, ListView, DetailView
-from articles.models import Article
+from articles.models import Article, ArticleTypeChoices
 from coverages. models import Coverage
 from investigations.models import Investigation
 from .models import Tags
@@ -10,7 +10,7 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        data['articles'] = Article.objects.all()
+        data['articles'] = Article.objects.filter(type=ArticleTypeChoices.ARTICLE)[:6]
         data['tags'] = Tags.objects.all()
         data["documentary_id"] = Documentary.objects.last().id
         return data
@@ -21,6 +21,6 @@ class TagDetailView(DetailView):
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         data['articles'] = Article.objects.filter(tag=self.get_object())
-        data['coverages'] = Coverage.objects.filter(tag=self.get_object())
-        data['investigations'] = Investigation.objects.filter(tag=self.get_object())
+        # data['coverages'] = Coverage.objects.filter(tag=self.get_object())
+        # data['investigations'] = Investigation.objects.filter(tag=self.get_object())
         return data
