dd7f6b68492a740d341776994b50e494075d2cbe
yasserAboelgheit <yasser@huspy.io>
2023-10-22 04:18:34 +0400

    add search and archive

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from home.views import HomePageView, TagDetailView
+from home.views import HomePageView, TagDetailView, search_view, archive_view
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-     path('tag/<int:pk>/', TagDetailView.as_view(), name='tag_detail'),
-
+    path('tag/<int:pk>/', TagDetailView.as_view(), name='tag_detail'),
+    path('search/', search_view, name='search_view'),
+    path('archive/', archive_view, name='archive_view'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,16 +1,31 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, DetailView
 from articles.models import Article, ArticleTypeChoices
 from .models import Tags
 from documentaries.models import Documentary
+import calendar
+from translate import Translator
+from datetime import datetime
+
 
 class HomePageView(TemplateView):
     template_name = 'home/home.html'
 
     def get_context_data(self, **kwargs):
+        translator = Translator(to_lang="Arabic")
+        dates_list = []
         data = super().get_context_data(**kwargs)
         data['articles'] = Article.objects.filter(type=ArticleTypeChoices.ARTICLE)[:6]
         data['tags'] = Tags.objects.all()
         data["documentary_id"] = Documentary.objects.last().id
+        dates = list(Article.objects.all().values_list("published_at",flat=True))
+        
+        for i in dates: dates_list.append(f"{calendar.month_name[i.month]} {i.year}")
+        # data["dates"] = dates_list
+        data["dates"] = dates_list
+        print(dates_list)
         return data
 class TagDetailView(DetailView):
     model = Tags
@@ -20,3 +35,18 @@ class TagDetailView(DetailView):
         data = super().get_context_data(**kwargs)
         data['articles'] = Article.objects.filter(tag=self.get_object())
         return data
+
+def search_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        articles = Article.objects.filter(content__contains=request.POST["search"])
+
+    return render(request, 'search/search.html', {'title': 'نتايج البحث', 'articles': articles})
+
+def archive_view(request, *args, **kwargs):
+
+    date_string = request.POST["archive-dropdown"]
+    date_format = "%B %Y"  
+
+    date = datetime.strptime(date_string, date_format)
+    articles = Article.objects.filter(published_at__year=date.year, published_at__month=date.month)
+    return render(request, 'search/search.html', {'title': request.POST["archive-dropdown"], 'articles': articles})
